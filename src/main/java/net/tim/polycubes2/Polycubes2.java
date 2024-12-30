5cd546b1f2cfe67327e9665380ce83e9d5be4530
Tim <timeo.golaz@epfl.ch>
2024-12-30 21:38:54 +0100

    Initial commit

---
+++ src/main/java/net/tim/polycubes2/Polycubes2.java
@@ -0,0 +1,14 @@
+package net.tim.polycubes2;
+
+import net.fabricmc.api.ModInitializer;
+
+import java.util.logging.Logger;
+
+public class Polycubes2 implements ModInitializer {
+    public static final String MOD_ID = "polycubes2";
+    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
